2e474e51a8ee2c303cb21594db2fde5a20d506da
Ryan Fadeau <ryan.fadeau@hotmail.fr>
2020-03-11 10:57:48 +0100

    feat(backend): Add sequelize-cli and sequelize ORM packages and create models and associations between them.

---
+++ Back/backend/models/Data.js
@@ -0,0 +1,37 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+    const Data = sequelize.define('Data', {
+        date: DataTypes.DATE,
+        pression: DataTypes.DECIMAL,
+        humidity: DataTypes.DECIMAL,
+        weather: DataTypes.STRING
+    }, {});
+    Data.associate = function(models) {
+        // associations can be defined here
+        Data.belongsTo(models.Temperature, {
+            foreignKey: 'id',
+            target_Key: 'temperatureId'
+        });
+
+        Data.belongsTo(models.Wind, {
+            foreignKey: 'id',
+            target_Key: 'windId'
+        });
+
+        Data.belongsTo(models.Precipitation, {
+            foreignKey: 'id',
+            target_Key: 'precipitationId'
+        });
+
+        Data.belongsTo(models.Cloud, {
+            foreignKey: 'id',
+            target_Key: 'cloudId'
+        });
+
+        Data.belongsTo(models.City, {
+            foreignKey: 'id',
+            target_Key: 'cityId'
+        });
+    };
+    return Data;
+};
