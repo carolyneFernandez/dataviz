88189ac0f3a0af82483cba57248eced6435b5f87
Carolyne <carolyne.fernandez1122@gmail.com>
2020-03-16 21:14:37 +0100

    feat(graph-temp): features's front

--- Front/src/app/app.module.ts
+++ Front/src/app/app.module.ts
@@ -3,14 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ChartsModule } from 'ng2-charts';
+import { GraphicComponent } from './graphic/graphic.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GraphicComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ChartsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ Front/src/app/graphic/graphic.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { PrevisionService } from '../services/prevision.service';
+
+
+@Component({
+  selector: 'app-graphic',
+  templateUrl: './graphic.component.html',
+  styleUrls: ['./graphic.component.scss']
+})
+export class GraphicComponent implements OnInit {
+  chartOptions = {
+    responsive: true
+  };
+
+  chartData = [
+    { data: [20, 25, 30, 25,35,21,20], label: 'Prevession' },
+    { data: [12, 10, 12, 10,12,10,10], label: 'Normal' }
+  ];
+
+  chartLabels = ['Lundi', 'Mardi', 'Mercredi', 'Jeudi','Vendredi','Samedi','Dimanche'];
+
+  onChartClick(event) {
+    console.log(event);
+  }
+  constructor(private url:PrevisionService) { 
+   // console.log(url.get_articles())
+  }
+
+  ngOnInit() {
+    
+  }
+ 
+
+}
