7fb7788fa193638dd932b21be298f76b8a9d40f6
Carolyne <carolyne.fernandez1122@gmail.com>
2020-03-23 21:42:28 +0100

    DATA 7

--- Front/src/app/graphic/graphic.component.ts
+++ Front/src/app/graphic/graphic.component.ts
@@ -13,8 +13,9 @@ export class GraphicComponent implements OnInit {
   };
 
   chartData = [
-    { data: [20, 25, 30, 25,35,21,20], label: 'Prevession' },
-    { data: [12, 10, 12, 10,12,10,10], label: 'Normal' }
+    { data: [20, 25, 30, 25,35,21,20], label: 'Min' },
+    { data: [12, 10, 12, 10,12,10,10], label: 'Normal' },
+    { data: [40, 40, 40, 30,40,40,40], label: 'Maxim' }
   ];
 
   chartLabels = ['Lundi', 'Mardi', 'Mercredi', 'Jeudi','Vendredi','Samedi','Dimanche'];
@@ -22,8 +23,18 @@ export class GraphicComponent implements OnInit {
   onChartClick(event) {
     console.log(event);
   }
+  public datos:any;//todo lo que nos responde persona
+  public listaPer:Object[]; //el array de objetos
   constructor(private url:WeatherService) { 
-    // console.log(url.get_articles())
+    
+    url.peti().subscribe(daticos =>{
+      console.log(daticos["results"]);
+      this.datos=daticos;
+      this.listaPer=this.datos.results;
+   });
+ 
+   console.log(this.datos);
+     
   }
 
   ngOnInit() {
--- Front/src/app/services/weather.service.ts
+++ Front/src/app/services/weather.service.ts
@@ -1,9 +1,29 @@
 import { Injectable } from '@angular/core';
+import { Observable} from 'rxjs';
+
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WeatherService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  private urlPersonajes:string="https://swapi.co/api/people/?format=json";
+
+  //CREAMOS UNA FUNCIOM DE PETICION
+  peti(){
+        //return this.http.get<any>(this.urlPersonajes);
+    return this.http.get(this.urlPersonajes);
+  }
+  searchWeatherInfo(city: string): Observable<any> {
+    const APPID = '7a211c68435846ab04153a9d815b09f3';
+    let url = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&APPID=' + APPID + '&units=metric';
+
+    return this.http.get<any>(url).pipe(map(res => res.json()));
+  
+}
+
 }
