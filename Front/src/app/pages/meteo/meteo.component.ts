495e23c6c10cbd3f12e4a9b75ef678dcc3a1bde6
ValentinVincent <valentinhov95@gmail.com>
2020-04-02 14:51:39 +0200

    DAT-8 : Graph Bar init

--- Front/src/app/pages/meteo/meteo.component.ts
+++ Front/src/app/pages/meteo/meteo.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef } from '@angular/core';
+import { Router } from '@angular/router';
+import { ChartsModule } from 'ng2-charts';
+import * as Chart from 'chart.js';
 
 @Component({
   selector: 'app-meteo',
@@ -6,10 +9,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./meteo.component.scss']
 })
 export class MeteoComponent implements OnInit {
+  public city: any;
 
-  constructor() { }
+  constructor(private router: Router, private elementRef: ElementRef) {
+
+    this.city = this.router.getCurrentNavigation().extras.state.myCity;
+  }
+
 
   ngOnInit(): void {
+    this.initChart(this.city);
+  }
+
+  initChart(city) {
+    console.log(city);
+    let htmlRef = this.elementRef.nativeElement.querySelector(`#myChart`);
+    var myBarChart = new Chart(htmlRef, {
+      type: 'bar',
+      data: {
+        labels: ["Vent violent", "Vent tempétueux", "Pluie", "Canicule", "Neige"],
+        datasets: [
+          {
+            label: "Nombre de jours",
+            backgroundColor: ["#3e95cd", "#8e5ea2", "#3cba9f", "#e8c3b9", "#c45850"],
+            data: [2, 1, 0, 5, 2]
+          }
+        ]
+      },
+      options: {
+        legend: { display: false },
+        title: {
+          display: true,
+          text: 'Nombre de jours par type de vents'
+        }
+      }
+    });
   }
 
 }
