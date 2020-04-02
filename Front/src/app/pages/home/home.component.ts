153f7bec5ba17e23b4abdc48e19c6d3ec99c5393
Emilie <vang.emilie25@gmail.com>
2020-04-02 21:35:32 +0200

    Feat(home-page): adding button-city for all cities in database

--- Front/src/app/pages/home/home.component.ts
+++ Front/src/app/pages/home/home.component.ts
@@ -1,27 +1,23 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { PrevisionService } from 'src/app/services/prevision.service';
+import { Component, OnInit, Input } from "@angular/core";
+import { PrevisionService } from "src/app/services/prevision.service";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.scss"],
 })
 export class HomeComponent implements OnInit {
   cities: any = [];
 
-  colorsButton: string[] = ['#7ED8F6', '#FF435B', '#FED843', '#FF7B4A'];
+  colorsButton: string[] = ["#7ED8F6", "#FF435B", "#FED843", "#FF7B4A"];
 
-  constructor(private previsionService: PrevisionService) { }
+  constructor(private previsionService: PrevisionService) {}
 
   ngOnInit(): void {
-   this.previsionService
-      .getCitiesAndTemperatures()
-      .subscribe(data => {
-        console.log(data);
-        for(let i in data){
-          this.cities.push(data[i]);
-        }
-      });
+    this.previsionService.getCitiesAndTemperatures().subscribe((data) => {
+      for (let i in data) {
+        this.cities.push(data[i]);
+      }
+    });
   }
-
 }
