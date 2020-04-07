1b723beb100871378ba984dfceb94e4592825fec
lgrimoin <leonie.grimoin@gmail.com>
2020-04-07 12:12:36 +0200

    responsive on meteo.component.html

--- Front/src/app/services/icon.service.ts
+++ Front/src/app/services/icon.service.ts
@@ -6,44 +6,61 @@ import { Injectable } from "@angular/core";
 export class IconService {
   constructor() {}
 
-  //TODO make icon
+  /* Select icon for humidity depending on humidity value */
   getIconHumidity(humidity) {
     if (humidity > 75) {
-      return "../assets/images/drop.svg";
+      return "../assets/images/Three_drops.svg";
     } else if (humidity >= 50) {
-      return "../assets/images/drop.svg";
+      return "../assets/images/Two_drops.svg";
+    } else if (humidity >= 25) {
+      return "../assets/images/One_drop.svg";
+    } else if (humidity >= 0) {
+      return "../assets/images/Transparent_drop.svg";
     } else {
-      return "../assets/images/transparent_drops.svg";
+      console.log("ERROR : Fail get icon humidity");
+      return "../assets/images/Two_drops.svg";
     }
   }
 
-  //TODO make icon
-  getIconWind(windName) { console.log(windName);
+  /* Select icon for wind depending on wind name */
+  getIconWind(windName) {
     switch (windName) {
       case "Gentle Breeze":
-        return "../assets/images/wind.svg";
-      case "Moderate breeze":
-        return "../assets/images/wind.svg";
-      case "Fresh Breeze":
-        return "../assets/images/wind.svg";
-      case "Light breeze":
-        return "../assets/images/wind.svg";
+        return "../assets/images/Gentle_Breeze.svg";
       case "Calm":
-        return "../assets/images/wind.svg";
+        return "../assets/images/Calm.svg";
+      case "Moderate breeze":
+        return "../assets/images/Moderate_breeze.svg";
+      case "Fresh Breeze":
+        return "../assets/images/Fresh_Breeze.svg";
+      case "Light breeze":
+        return "../assets/images/Light_breeze.svg";
+
       default:
         console.log("ERROR : Fail get icon wind");
-        break;
+        return "../assets/images/Gentle_Breeze.svg";
     }
   }
 
-  //TODO make icon /* Select icon for the pressure */
+  /* Select icon for pressure depending on pressure value */
   getIconPressure(pressure) {
-    if (pressure > 75) {
-      return "../assets/images/drop.svg";
-    } else if (pressure >= 50) {
-      return "../assets/images/drop.svg";
+    if (pressure < 960) {
+      return "../assets/images/pressure_960.svg";
+    } else if (pressure <= 980) {
+      return "../assets/images/pressure_980.svg";
+    } else if (pressure <= 995) {
+      return "../assets/images/pressure_995.svg";
+    } else if (pressure <= 1010) {
+      return "../assets/images/pressure_1010.svg";
+    } else if (pressure <= 1025) {
+      return "../assets/images/pressure_1025.svg";
+    } else if (pressure <= 1040) {
+      return "../assets/images/pressure_1040.svg";
+    } else if (pressure <= 1060) {
+      return "../assets/images/pressure_1060.svg";
     } else {
-      return "../assets/images/transparent_drops.svg";
+      console.log("ERROR : Fail get icon pressure");
+      return "../assets/images/pressure_1010.svg";
     }
   }
 }
